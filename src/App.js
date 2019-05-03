e5e6208cc9b44bc38baa139a260f9ff947ed0098
Ryan Mok <mokryan17@gmail.com>
2019-05-02 23:30:11 -0500

    Added intro code to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import "./App.css";
+import "./Person/Person.css";
 
 function App() {
+  // JSX elements will be added here by means of components!
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm the beginning of a basic React App!</h1>
+      <h2>
+        This is a pretty bare bones example, but there's still going to be some
+        interesting stuff happening here! I promise!
+      </h2>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import "../Person/Person.css";
 
 const person = props => {
